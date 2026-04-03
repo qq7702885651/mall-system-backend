9203332b4cc9938ba9d030a96dfb42649c0fdb8d
黄思维 <14870082+huang-siwei0911@user.noreply.gitee.com>
2026-04-03 21:29:13 +0800

    修改目录结构

---
+++ mall-model/src/main/java/com/siwei/App.java
@@ -0,0 +1,13 @@
+package com.siwei;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
